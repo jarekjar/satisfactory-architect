f58e2a2c4bc0fa75311c21a83461e7cbd14663d2
jared <jaredkjar@gmail.com>
2022-10-12 16:06:21 -0700

    added

--- src/components/home/home.jsx
+++ src/components/home/home.jsx
@@ -9,10 +9,9 @@ const Home = () => {
 
   return (
     <div className="">
-        <h1 className="home">Satisfactory Architect</h1>
+      <h1 className="home">Satisfactory Architect (wip)</h1>
       {Object.entries(data.items).map(([className, item]) => (
         <div className="item-container" key={className}>
-          {console.log(className)}
           <img className="item-icon" src={process.env.PUBLIC_URL + `/images/${item?.slug}_64.png`} alt={item?.slug}></img>
           <div className="item-info-container">
             <div className="item-title">{item?.name}</div>
