2fda6148d47b0586413e38603cc54d5da75b400f
jared <jaredkjar@gmail.com>
2022-10-12 13:57:40 -0700

    first commit

--- src/components/home/home.jsx
+++ src/components/home/home.jsx
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from '../../images/logo.svg';
+import './home.css';
 
-function App() {
+const Home = () => {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
+          href="http://google.com"
         >
-          Learn React
+          Satisfactory Architect (built in react v18.2)
         </a>
       </header>
     </div>
   );
 }
 
-export default App;
+export default Home;
