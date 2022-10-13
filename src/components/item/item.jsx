199a17f92ade864f765caca98eec48f996ab71ae
jared <jaredkjar@gmail.com>
2022-10-12 17:25:35 -0700

    updated

--- src/components/item/item.jsx
+++ src/components/item/item.jsx
@@ -3,10 +3,6 @@ import List from '@mui/material/List';
 import ListItem from '@mui/material/ListItem';
 import ListItemText from '@mui/material/ListItemText';
 import ListItemAvatar from '@mui/material/ListItemAvatar';
-import Avatar from '@mui/material/Avatar';
-import ImageIcon from '@mui/icons-material/Image';
-import WorkIcon from '@mui/icons-material/Work';
-import BeachAccessIcon from '@mui/icons-material/BeachAccess';
 import Divider from '@mui/material/Divider';
 
 const ItemList = ({data}) => {
